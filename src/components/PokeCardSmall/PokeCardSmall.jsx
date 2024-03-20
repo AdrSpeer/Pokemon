fcdb522e9d210bde28fd7fd452886793f4dc1c0a
StefanHolz1997 <Stefanholz97@web.de>
2024-03-20 09:17:11 +0100

    Small_Card_Link

--- src/components/PokeCardSmall/PokeCardSmall.jsx
+++ src/components/PokeCardSmall/PokeCardSmall.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import "./PokeCardSmall.css";
 
 const PokeCardSmall = ({ url }) => {
@@ -11,32 +12,32 @@ const PokeCardSmall = ({ url }) => {
       .catch((error) => console.error("Fehler im Detail Pokemon Fetch", error));
   }, []);
 
-  console.log(pokemonData);
-
   return (
-    <section className="render-pokemon">
-      {pokemonData ? (
-        <div className="poke-card">
-          <img
-            src={pokemonData?.sprites.other["official-artwork"].front_default}
-            alt=""
-          />
-          <div className="poke-name">
-            <p>
-              #
-              {pokemonData.id < 10
-                ? "00" + pokemonData.id
-                : pokemonData.id < 100 && pokemonData.id
-                ? "0" + pokemonData.id
-                : pokemonData.id}
-            </p>
-            <p>{pokemonData?.name}</p>
+    <Link to={`/details/${pokemonData?.species?.name}`}>
+      <section className="render-pokemon">
+        {pokemonData ? (
+          <div className="poke-card">
+            <img
+              src={pokemonData?.sprites.other["official-artwork"].front_default}
+              alt=""
+            />
+            <div className="poke-name">
+              <p>
+                #
+                {pokemonData.id < 10
+                  ? "00" + pokemonData.id
+                  : pokemonData.id < 100 && pokemonData.id
+                  ? "0" + pokemonData.id
+                  : pokemonData.id}
+              </p>
+              <p>{pokemonData?.name}</p>
+            </div>
           </div>
-        </div>
-      ) : (
-        <p>loading....</p>
-      )}
-    </section>
+        ) : (
+          <p>loading....</p>
+        )}
+      </section>
+    </Link>
   );
 };
 
